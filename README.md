d0ec7e75fa6224a0ef05443d40bc90f40f4482a8
zvezdochiot <mykaralw@yandex.ru>
2019-06-30 11:05:57 +0300

    0.0.3: reformatted code and updated error handlers and documentation

--- README.md
+++ README.md
@@ -10,27 +10,52 @@ USAGE
 =====
 
 Origin:  
-![Origin](images/lena.q100.jpg)
+![Origin](images/lena.q100.jpg)  
+[211044]
 
 ```
 ./jpegquant lena.q100.jpg lena.q100.jq50.jpg 50
 ```
-![Quant=50](images/lena.q100.jq50.jpg) ![butteraugli=50](images/lena.q100.jq50.butteraugli.jpg)
+![Quant=50](images/lena.q100.jq50.jpg)  
+[176313]  
+![butteraugli=50](images/lena.q100.jq50.butteraugli.jpg)  
+butteraugli: 0.782326
 
 ```
 ./jpegquant lena.q100.jpg lena.q100.jq25.jpg 25
 ```
-![Quant=25](images/lena.q100.jq25.jpg) ![butteraugli=25](images/lena.q100.jq25.butteraugli.jpg)
+![Quant=25](images/lena.q100.jq25.jpg)  
+[110952]  
+![butteraugli=25](images/lena.q100.jq25.butteraugli.jpg)  
+butteraugli: 1.688544
 
 ```
 ./jpegquant lena.q100.jpg lena.q100.jq10.jpg 10
 ```
-![Quant=10](images/lena.q100.jq10.jpg) ![butteraugli=10](images/lena.q100.jq10.butteraugli.jpg)
+![Quant=10](images/lena.q100.jq10.jpg)  
+[45875]  
+![butteraugli=10](images/lena.q100.jq10.butteraugli.jpg)  
+butteraugli: 4.625142
 
 ```
 ./jpegquant lena.q100.jpg lena.q100.jq05.jpg 5
 ```
-![Quant=05](images/lena.q100.jq05.jpg) ![butteraugli=05](images/lena.q100.jq05.butteraugli.jpg)
+![Quant=05](images/lena.q100.jq05.jpg)  
+[26211]  
+![butteraugli=05](images/lena.q100.jq05.butteraugli.jpg)  
+butteraugli: 7.342572
+
+See also
+========
+
+* https://github.com/kud/jpegrescan
+* https://github.com/ImageProcessing-ElectronicPublications/jpeg-recompress
+
+Authors
+=======
+
+Copyright (C) 2012, Owen Campbell-Moore.  
+Modify 2019, zvezdochiot
 
 HomePage
 ========
--- jpegquant.c
+++ jpegquant.c
@@ -3,6 +3,7 @@
  * based dct.c
  *
  * Copyright (C) 2012, Owen Campbell-Moore.
+ * Modify 2019, zvezdochiot
  *
  * This program allows you to take JPEG, modify it's DCT coefficients and
  * then output the new coefficients to a new JPEG.
@@ -20,21 +21,20 @@
 #include <jpeglib.h>
 #define SIZEOF(object)((size_t) sizeof(object))
 #ifndef EXIT_FAILURE
-#define EXIT_FAILURE  1
+#define EXIT_FAILURE 1
 #endif
 #ifndef EXIT_SUCCESS
-#define EXIT_SUCCESS  0
+#define EXIT_SUCCESS 0
 #endif
 #ifndef EXIT_WARNING
-#define EXIT_WARNING  2
+#define EXIT_WARNING 2
 #endif
-//#include "cdjpeg.h"     /* Common decls for compressing and decompressing jpegs */
 
 void usage (char *cmd)
 /* complain about bad command line */
 {
-  fprintf(stderr, "\nusage: %s input.jpg output.jpg [quant=100]\n\n", cmd);
-  exit(EXIT_FAILURE);
+    fprintf(stderr, "\nusage: %s input.jpg output.jpg [quant=100]\n\n", cmd);
+    exit(EXIT_FAILURE);
 }
 
 /*
@@ -44,147 +44,154 @@ void usage (char *cmd)
 int
 main (int argc, char **argv)
 {
-  struct jpeg_decompress_struct inputinfo;
-  struct jpeg_compress_struct outputinfo;
-  struct jpeg_error_mgr jerr;
-  jvirt_barray_ptr *coef_arrays;
-  JDIMENSION i, compnum, rownum, blocknum;
-  JBLOCKARRAY coef_buffers[MAX_COMPONENTS];
-  JBLOCKARRAY row_ptrs[MAX_COMPONENTS];
-  FILE * input_file;
-  FILE * output_file;
+    struct jpeg_decompress_struct inputinfo;
+    struct jpeg_compress_struct outputinfo;
+    struct jpeg_error_mgr jerr;
+    jvirt_barray_ptr *coef_arrays;
+    JDIMENSION i, compnum, rownum, blocknum;
+    JBLOCKARRAY coef_buffers[MAX_COMPONENTS];
+    JBLOCKARRAY row_ptrs[MAX_COMPONENTS];
+    FILE * input_file;
+    FILE * output_file;
 
-  /* Handle arguments */
-  if (argc < 3 || argc > 4) usage(argv[0]);
-  char *inputname;
-  inputname = argv[1];
-  char *outputname;
-  outputname = argv[2];
-  double recoef, coeferr, sumce, numc, quant = 100.0;
-  if (argc == 4) quant = atof(argv[3]);
+    /* Handle arguments */
+    if (argc < 3 || argc > 4) usage(argv[0]);
+    char *inputname;
+    inputname = argv[1];
+    char *outputname;
+    outputname = argv[2];
+    double recoef, coeferr, sumce, numc, iquant, quant = 100.0;
+    if (argc == 4) quant = atof(argv[3]);
+    if (strcmp(inputname, outputname) == 0)
+    {
+        fprintf(stderr, "ERROR: Cannot output to input %s\n", inputname);
+        exit(EXIT_FAILURE);
+    }
+    /* Open the input and output files */
+    if ((input_file = fopen(inputname, "rb")) == NULL)
+    {
+        fprintf(stderr, "ERROR: Can't open %s\n", inputname);
+        exit(EXIT_FAILURE);
+    }
+    if ((output_file = fopen(outputname, "wb")) == NULL)
+    {
+        fprintf(stderr, "ERROR: Can't open %s\n", outputname);
+        exit(EXIT_FAILURE);
+    }
 
-  /* Open the input and output files */
-  if ((input_file = fopen(inputname, "rb")) == NULL) {
-      fprintf(stderr, "Can't open %s\n", inputname);
-      exit(EXIT_FAILURE);
-  }
-  if ((output_file = fopen(outputname, "wb")) == NULL) {
-    fprintf(stderr, "Can't open %s\n", outputname);
-    exit(EXIT_FAILURE);
-  }
+    /* Initialize the JPEG compression and decompression objects with default error handling. */
+    inputinfo.err = jpeg_std_error(&jerr);
+    jpeg_create_decompress(&inputinfo);
+    outputinfo.err = jpeg_std_error(&jerr);
+    jpeg_create_compress(&outputinfo);
 
-  /* Initialize the JPEG compression and decompression objects with default error handling. */
-  inputinfo.err = jpeg_std_error(&jerr);
-  jpeg_create_decompress(&inputinfo);
-  outputinfo.err = jpeg_std_error(&jerr);
-  jpeg_create_compress(&outputinfo);
+    /* Specify data source for decompression and recompression */
+    jpeg_stdio_src(&inputinfo, input_file);
+    jpeg_stdio_dest(&outputinfo, output_file);
 
-  /* Specify data source for decompression and recompression */
-  jpeg_stdio_src(&inputinfo, input_file);
-  jpeg_stdio_dest(&outputinfo, output_file);
+    /* Read file header */
+    (void) jpeg_read_header(&inputinfo, TRUE);
+    printf("Read DCT coefficients successfully written to %s\n", inputname);
 
-  /* Read file header */
-  (void) jpeg_read_header(&inputinfo, TRUE);
-  printf("Read DCT coefficients successfully written to %s\n", inputname);
-
-  /* Allocate memory for reading out DCT coeffs */
-  for (compnum=0; compnum<inputinfo.num_components; compnum++)
-    coef_buffers[compnum] = ((&inputinfo)->mem->alloc_barray) 
+    /* Allocate memory for reading out DCT coeffs */
+    for (compnum=0; compnum<inputinfo.num_components; compnum++)
+        coef_buffers[compnum] = ((&inputinfo)->mem->alloc_barray) 
                             ((j_common_ptr) &inputinfo, JPOOL_IMAGE,
                              inputinfo.comp_info[compnum].width_in_blocks,
                              inputinfo.comp_info[compnum].height_in_blocks);
 
-  /* Read input file as DCT coeffs */
-  coef_arrays = jpeg_read_coefficients(&inputinfo);
+    /* Read input file as DCT coeffs */
+    coef_arrays = jpeg_read_coefficients(&inputinfo);
 
-  /* Copy compression parameters from the input file to the output file */
-  jpeg_copy_critical_parameters(&inputinfo, &outputinfo);
+    /* Copy compression parameters from the input file to the output file */
+    jpeg_copy_critical_parameters(&inputinfo, &outputinfo);
 
-  /* Copy DCT coeffs to a new array */
-  int num_components = inputinfo.num_components;
-  size_t block_row_size[num_components];
-  int width_in_blocks[num_components];
-  int height_in_blocks[num_components];
-  for (compnum=0; compnum<num_components; compnum++)
-  {
-    height_in_blocks[compnum] = inputinfo.comp_info[compnum].height_in_blocks;
-    width_in_blocks[compnum] = inputinfo.comp_info[compnum].width_in_blocks;
-    block_row_size[compnum] = (size_t) SIZEOF(JCOEF)*DCTSIZE2*width_in_blocks[compnum];
-    for (rownum=0; rownum<height_in_blocks[compnum]; rownum++)
+    /* Copy DCT coeffs to a new array */
+    int num_components = inputinfo.num_components;
+    size_t block_row_size[num_components];
+    int width_in_blocks[num_components];
+    int height_in_blocks[num_components];
+    for (compnum=0; compnum<num_components; compnum++)
     {
-      row_ptrs[compnum] = ((&inputinfo)->mem->access_virt_barray) 
-                          ((j_common_ptr) &inputinfo, coef_arrays[compnum], 
-                            rownum, (JDIMENSION) 1, FALSE);
-      for (blocknum=0; blocknum<width_in_blocks[compnum]; blocknum++)
-      {
-        for (i=0; i<DCTSIZE2; i++)
+        height_in_blocks[compnum] = inputinfo.comp_info[compnum].height_in_blocks;
+        width_in_blocks[compnum] = inputinfo.comp_info[compnum].width_in_blocks;
+        block_row_size[compnum] = (size_t) SIZEOF(JCOEF)*DCTSIZE2*width_in_blocks[compnum];
+        for (rownum=0; rownum<height_in_blocks[compnum]; rownum++)
         {
-          coef_buffers[compnum][rownum][blocknum][i] = row_ptrs[compnum][0][blocknum][i];
+            row_ptrs[compnum] = ((&inputinfo)->mem->access_virt_barray) 
+                                ((j_common_ptr) &inputinfo, coef_arrays[compnum], 
+                                    rownum, (JDIMENSION) 1, FALSE);
+            for (blocknum=0; blocknum<width_in_blocks[compnum]; blocknum++)
+            {
+                for (i=0; i<DCTSIZE2; i++)
+                {
+                    coef_buffers[compnum][rownum][blocknum][i] = row_ptrs[compnum][0][blocknum][i];
+                }
+            }
         }
-      }
     }
-  }
 
-  printf("ReQuant = %f%%\n", quant);
-  quant /= 100.0;
-  sumce = 0.0;
-  /* Print out or modify DCT coefficients */
-  for (compnum=0; compnum<num_components; compnum++)
-  {
-    for (rownum=0; rownum<height_in_blocks[compnum]; rownum++)
+    printf("Quant = %f%%\n", quant);
+    quant /= 100.0;
+    iquant = (quant == 0) ? 1.0 : 1.0 / quant;
+    sumce = 0.0;
+    /* Print out or modify DCT coefficients */
+    for (compnum=0; compnum<num_components; compnum++)
     {
-      for (blocknum=0; blocknum<width_in_blocks[compnum]; blocknum++)
-      {
-        //printf("\n\nComponent: %i, Row:%i, Column: %i\n", compnum, rownum, blocknum);
-        for (i=0; i<DCTSIZE2; i++)
+        for (rownum=0; rownum<height_in_blocks[compnum]; rownum++)
         {
-          recoef = coef_buffers[compnum][rownum][blocknum][i];
-          coeferr = recoef;
-          recoef = (int)(recoef * quant);
-          recoef = (int)(recoef / quant);
-          coeferr -= recoef;
-          if (coeferr < 0) coeferr = -coeferr;
-          sumce += coeferr;
-          numc++;
-          coef_buffers[compnum][rownum][blocknum][i] = recoef;
-          //printf("%i,", coef_buffers[compnum][rownum][blocknum][i]);
+            for (blocknum=0; blocknum<width_in_blocks[compnum]; blocknum++)
+            {
+                //printf("\n\nComponent: %i, Row:%i, Column: %i\n", compnum, rownum, blocknum);
+                for (i=0; i<DCTSIZE2; i++)
+                {
+                    recoef = coef_buffers[compnum][rownum][blocknum][i];
+                    coeferr = recoef;
+                    recoef = (int)(recoef * quant);
+                    recoef = (int)(recoef * iquant);
+                    coeferr -= recoef;
+                    if (coeferr < 0) coeferr = -coeferr;
+                    sumce += coeferr;
+                    numc++;
+                    coef_buffers[compnum][rownum][blocknum][i] = recoef;
+                    //printf("%i,", coef_buffers[compnum][rownum][blocknum][i]);
+                }
+            }
         }
-      }
     }
-  }
-  if (numc > 0) sumce /= numc;
-  printf("QuantErr = %f\n", sumce);
-  //printf("\n\n");
+    if (numc > 0) sumce /= numc;
+    printf("QuantErr = %f\n", sumce);
+    //printf("\n\n");
 
-  /* Output the new DCT coeffs to a JPEG file */
-  for (compnum=0; compnum<num_components; compnum++)
-  {
-    for (rownum=0; rownum<height_in_blocks[compnum]; rownum++)
+    /* Output the new DCT coeffs to a JPEG file */
+    for (compnum=0; compnum<num_components; compnum++)
     {
-      row_ptrs[compnum] = ((&outputinfo)->mem->access_virt_barray) 
-                          ((j_common_ptr) &outputinfo, coef_arrays[compnum], 
-                           rownum, (JDIMENSION) 1, TRUE);
-      memcpy(row_ptrs[compnum][0][0], 
-             coef_buffers[compnum][rownum][0],
-             block_row_size[compnum]);
+        for (rownum=0; rownum<height_in_blocks[compnum]; rownum++)
+        {
+            row_ptrs[compnum] = ((&outputinfo)->mem->access_virt_barray) 
+                                ((j_common_ptr) &outputinfo, coef_arrays[compnum], 
+                                 rownum, (JDIMENSION) 1, TRUE);
+            memcpy(row_ptrs[compnum][0][0], 
+                 coef_buffers[compnum][rownum][0],
+                 block_row_size[compnum]);
+        }
     }
-  }
 
-  /* Write to the output file */
-  jpeg_write_coefficients(&outputinfo, coef_arrays);
+    /* Write to the output file */
+    jpeg_write_coefficients(&outputinfo, coef_arrays);
 
-  /* Finish compression and release memory */
-  jpeg_finish_compress(&outputinfo);
-  jpeg_destroy_compress(&outputinfo);
-  jpeg_finish_decompress(&inputinfo);
-  jpeg_destroy_decompress(&inputinfo);
+    /* Finish compression and release memory */
+    jpeg_finish_compress(&outputinfo);
+    jpeg_destroy_compress(&outputinfo);
+    jpeg_finish_decompress(&inputinfo);
+    jpeg_destroy_decompress(&inputinfo);
 
-  /* Close files */
-  fclose(input_file);
-  fclose(output_file);
+    /* Close files */
+    fclose(input_file);
+    fclose(output_file);
 
-  /* All done. */
-  printf("New DCT coefficients successfully written to %s\n\n", outputname);
-  exit(jerr.num_warnings ? EXIT_WARNING : EXIT_SUCCESS);
-  return 0;         /* suppress no-return-value warnings */
+    /* All done. */
+    printf("New DCT coefficients successfully written to %s\n\n", outputname);
+    exit(jerr.num_warnings ? EXIT_WARNING : EXIT_SUCCESS);
+    return 0; /* suppress no-return-value warnings */
 }
